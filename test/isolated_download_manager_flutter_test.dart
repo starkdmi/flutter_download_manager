1ae090b1118fbb5e6a1bd6c5885a747e39bcad3e
Dmitry Starkov <21260939+starkdmi@users.noreply.github.com>
2022-10-04 14:41:53 +0300

    Rename test file

---
+++ test/isolated_download_manager_flutter_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:isolated_download_manager_flutter/isolated_download_manager_flutter.dart';
+
+void main() {
+  // To be continued..
+}
